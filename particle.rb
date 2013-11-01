0b24c1b8a919e1ecd7bde0aaf7c1ae8f53efd81d
ilya.kuzmin <zxlmorf@gmail.com>
2013-11-01 18:35:01 +0400

    Update SPH algorithm (draft)

--- particle.rb
+++ particle.rb
@@ -24,7 +24,15 @@ end
 
 G = 40
 PARTICLE_RADIUS = 3
-CONNECT_RADIUS = 50
+CONNECT_RADIUS = 15
+CONNECT_RADIUS_2 = CONNECT_RADIUS * CONNECT_RADIUS
+DIAMETR = CONNECT_RADIUS * 2
+CELLS_X = 640 / DIAMETR
+CELLS_Y = 480 / DIAMETR
+$glob_K = 0.1
+$glob_rest_ro = 0.5
+$glob_K_near = 0.5
+$show_radius = 0
 
 class Particles
   attr_accessor :particles
@@ -38,6 +46,15 @@ class Particles
   end
 
   def update
+    # Greed
+    @grid = []
+    @particles.each do |particle|
+      x, y = particle.pos_in_grid
+      @grid[x] ||= []
+      @grid[x][y] ||= []
+      @grid[x][y] << particle
+    end
+
     @particles.each do |particle|
       particle.ro = 0
       particle.ro_near = 0
@@ -45,54 +62,78 @@ class Particles
       particle.press_near = 0
     end
 
-    # Boost
-    @particles.each do |particle|
-      (@particles - [particle]).each do |particle2|
-        r_square = (particle2.position.x - particle.position.x)**2 + (particle2.position.y - particle.position.y)**2
-        r = Math.sqrt(r_square)
+    # @particles.each do |particle|
+      # (@particles - [particle]).each do |particle2|
+    CELLS_X.times do |x|
+      CELLS_Y.times do |y|
+        next unless @grid[x].is_a?(Array) && @grid[x][y].is_a?(Array)
 
-        if r < CONNECT_RADIUS
+        big_cell = []
+        (-1..1).to_a.each do |x2|
+          (-1..1).to_a.each do |y2|
+            big_cell << @grid[x-x2][y-y2] if @grid[x-x2] && @grid[x-x2][y-y2]
+          end
+        end
+        big_cell.flatten!.compact!
 
-          q = 1 - (r / CONNECT_RADIUS)
-          q2 = q * q
+        @grid[x][y].each do |particle|
+          (big_cell - [particle]).each do |particle2|
 
-          particle.ro += q2
-          particle.ro_near += q2 * q
+            r_square = (particle2.position.x - particle.position.x)**2 + (particle2.position.y - particle.position.y)**2
+
+            if r_square < CONNECT_RADIUS_2
+
+              q = (r_square / CONNECT_RADIUS_2)
+
+              if q < 1
+                q = 1 - q
+                q2 = q * q
+
+                particle.ro += q2
+                particle.ro_near += q2 * q
+              end
+            end
+          end
+
+          particle.press = $glob_K * (particle.ro - $glob_rest_ro) # K, rest_ro
+          particle.press_near = $glob_K_near * particle.ro_near # K_near
+
+
+          sx = 0
+          sy = 0
+          (big_cell - [particle]).each do |particle2|
+            r_square = (particle2.position.x - particle.position.x)**2 + (particle2.position.y - particle.position.y)**2
+            r = Math.sqrt(r_square)
+
+            if r < CONNECT_RADIUS
+
+              if r / CONNECT_RADIUS < 1
+              factor_x = particle2.position.x - particle.position.x
+              factor_y = particle2.position.y - particle.position.y
+
+              eps = 0.01
+              if (factor_x < eps) || (factor_y < eps)
+                particle.position.x += rand(100)/10000.0 * PlusMinus.get
+                particle.position.y += rand(100)/10000.0 * PlusMinus.get
+              end
+
+              q = 1 - (r / CONNECT_RADIUS)
+              tmp = q*((particle.press) + (particle.press_near)*q)/r
+
+              visc = 2
+
+              particle2.boost.x += factor_x * tmp * visc / 2.0
+              particle2.boost.y += factor_y * tmp * visc / 2.0
+              sx -= factor_x * tmp * visc / 2.0
+              sy -= factor_y * tmp * visc / 2.0
+              end
+            end
+          end
+
+          particle.boost.x += sx
+          particle.boost.y += sy
         end
       end
-    # end
-
-    # @particles.each do |particle|
-      particle.press = 10 * (particle.ro - 5) # K, rest_ro
-      particle.press_near = 20 * particle.ro_near # K_near
-    # end
-
-    # @particles.each do |particle|
-      sx = 0
-      sy = 0
-      (@particles - [particle]).each do |particle2|
-        r_square = (particle2.position.x - particle.position.x)**2 + (particle2.position.y - particle.position.y)**2
-        r = Math.sqrt(r_square)
-
-        if r < CONNECT_RADIUS
-          factor_x = (particle2.position.x - particle.position.x)
-          factor_y = (particle2.position.y - particle.position.y)
-
-          q = 1 - (r / CONNECT_RADIUS)
-          # tmp = q*((particle.press + particle2.press) + (particle.press_near + particle2.press_near)*q)/r
-          tmp = q*((particle.press) + (particle.press_near)*q)/(r*2)
-
-          visc = 0.1
-
-          particle2.boost.x += factor_x * tmp * visc
-          particle2.boost.y += factor_y * tmp * visc
-          sx += factor_x * tmp * visc
-          sy += factor_y * tmp * visc
-        end
-      end
-
-      particle.boost.x -= sx
-      particle.boost.y -= sy
     end
 
     # Position
@@ -101,30 +142,29 @@ class Particles
       particle.position += particle.boost
 
       if particle.position.y > 400
-        particle.boost.y *= -0.1
-        particle.position.y = 400
+        particle.boost.y *= -0.3
+        particle.boost.y -= 0.5
+        particle.position.y = 400.0
       end
 
-      if particle.position.x > 500
-        particle.boost.x *= -0.1
-        particle.position.x = 500
+      if particle.position.x > 501
+        particle.boost.x *= -0.3
+        particle.position.x = 500.0
       end
 
-      if particle.position.x < 50
-        particle.boost.x *= -0.1
-        particle.position.x = 50
+      if particle.position.x < 49
+        particle.boost.x *= -0.3
+        particle.position.x = 50.0
+      end
+      @count_d ||= 0
+      if particle.position.x > 640 || particle.position.x < 0 ||
+          particle.position.y > 480 ||
+          particle.position.x.nan? || particle.position.y.nan?
+        @particles.delete(particle)
+        @count_d += 1
+        puts "DELETED #{@count_d}"
       end
     end
-
-    # @particles.each do |particle|
-    #   i = 0
-    #   (@particles - [particle]).each do |particle2|
-    #     if particle.position == particle2.position
-    #       # puts "COLL #{i}"
-    #       i += 1
-    #     end
-    #   end
-    # end
   end
 
   def draw
@@ -153,7 +193,7 @@ class Particle
 
     @r_square = 0
 
-    # @image3  = Gosu::Image.new($window, Circle.new(CONNECT_RADIUS), true)
+    @image3  = Gosu::Image.new($window, Circle.new(CONNECT_RADIUS), true)
     # @image4  = Gosu::Image.new($window, Circle.new(CONNECT_RADIUS-1, Circle::BLACK), true)
 
     @image  = Gosu::Image.new($window, Circle.new(PARTICLE_RADIUS), true)
@@ -168,14 +208,24 @@ class Particle
 
     @image.draw(@position.x, @position.y, 1);
     @image2.draw(@position.x+1, @position.y+1, 1);
+
+    if $show_radius == 1
+      @image3.draw(x, y, 1);
+    end
   end
 
   def update
 
   end
 
+  def pos_in_grid
+    return (position.x / DIAMETR).round, (position.y / DIAMETR).round
+  end
+
   def debug
-    "X: #{@position.x.round(1)}, Y: #{@position.y.round(1)}, R: #{radius.round(1)}, "
+    "X: #{@position.x.round(1)}, Y: #{@position.y.round(1)}"+ #, R: #{radius.round(1)}, "
+    "ro: #{self.ro.round(1)}, ro_near: #{self.ro_near.round(1)}"+ #, R: #{radius.round(1)}, "
+    "press: #{self.press.round(1)}, press_near: #{self.press_near.round(1)}" #, R: #{radius.round(1)}, "
     # "B_X: #{@boost.x.round(1)}, B_Y: #{@boost.y.round(1)}, "+
     # "V_X: #{@velocity.x.round(1)}, V_Y: #{@velocity.y.round(1)}, "
   end
--- window.rb
+++ window.rb
@@ -16,69 +16,76 @@ class GameWindow < Gosu::Window
   end
   
   def update
-
-    if @score == 0
-      # @particles.new_particle(x: 200, y: 350)
-      # @particles.new_particle(x: 210, y: 350)
-      # @particles.new_particle(x: 200, y: 340)
-      # @particles.new_particle(x: 215, y: 350)
-      # @particles.new_particle(x: 220, y: 350)
-      # @particles.new_particle(x: 225, y: 350)
-      # @particles.new_particle(x: 230, y: 350)
-    end
-
-    @score += 1
-
-
     if button_down?(Gosu::MsLeft)
-      # return if @button_pressed
-      # @button_pressed = true
-
       @particles.new_particle(x: mouse_x + rand(100) - 50, y: mouse_y + rand(100) - 50)
-    else
-      # @button_pressed = false
     end
 
     if button_down?(Gosu::KbA)
       return if @button_pressed2
       @button_pressed2 = true
 
-      # @particles.update
-
       @particles.particles.clear
-      @particles.new_particle(x: 200, y: 350)
-      @particles.new_particle(x: 210, y: 350)
-      @particles.new_particle(x: 200, y: 340)
-      @particles.new_particle(x: 210, y: 340)
+      draw_seeds
     else
       @button_pressed2 = false
-    end
-    @particles.update
-
-    if button_down?(Gosu::KbS)
-      return if @button_pressed
-      @button_pressed = true
-      
-      @particles.update
-    else
-      @button_pressed = false
     end
 
     if button_down?(Gosu::KbD)
       @particles.update
     end
 
-
-    if button_down?(Gosu::KbS)
-      @particles.particles.each do |particle|
-        particle.velocity.x *= 0.9
-        particle.velocity.y *= 0.9
-      end
+    if button_down?(Gosu::KbY)
+      $glob_K += 0.05
     end
+    if button_down?(Gosu::KbH)
+      $glob_K -= 0.05
+    end
+    if button_down?(Gosu::KbU)
+      $glob_rest_ro += 0.05
+    end
+    if button_down?(Gosu::KbJ)
+      $glob_rest_ro -= 0.05
+    end
+    if button_down?(Gosu::KbI)
+      $glob_K_near += 0.05
+    end
+    if button_down?(Gosu::KbK)
+      $glob_K_near -= 0.05
+    end
+
+    if button_down?(Gosu::KbQ)
+      return if @button_pressed
+      @button_pressed = true
+
+      $show_radius = 1 - $show_radius
+    else
+      @button_pressed = false
+    end
+
+    @particles.update
+  end
+
+  def draw_seeds
+    k = CONNECT_RADIUS * 2
+    x_count = 300 / k
+    y_count = 200 / k
+    x = 1
+    while (x < x_count )
+      y = 1
+      while (y < y_count)
+        @particles.new_particle(x: 60 + x * k, y: 200 + y * k)
+        y += 1
+      end
+      x += 1
+    end
+    @particles.new_particle(x: 305, y: 305)
   end
   
   def draw
     $font.draw("Particles: #{@particles.count}", 10, 10, 2, 1.0, 1.0, 0xffffff00)
+    $font.draw("K: #{$glob_K}, rest_ro: #{$glob_rest_ro}, K_near: #{$glob_K_near}", 10, 28, 2, 1.0, 1.0, 0xffffff00)
+
+    $font.draw("FPS: "+Gosu.fps.to_s, 580, 30, 2, 1.0, 1.0, 0xffffff00)
 
     @particles.draw
   end
